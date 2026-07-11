55e16f8910d88deeb58eb4ed35d475e9db2becd0
noctis8 <reinhard19.fak@gmail.com>
2026-07-11 08:49:13 +0100

    Fix prisma errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { APP_GUARD } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-// import { AccessTokenGuard } from './auth/guard/access-token.guard';
+import { AccessTokenGuard } from './auth/guard/access-token.guard';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
@@ -25,7 +25,7 @@ import { PrismaModule } from './prisma/prisma.module';
 
     // Every route is protected by default.
     // Use @Public() to opt out individual routes.
-    // { provide: APP_GUARD, useClass: AccessTokenGuard },
+    { provide: APP_GUARD, useClass: AccessTokenGuard },
   ],
 })
 export class AppModule {}
